955627e4fd4d3de18c0561239a80ccc50b8c69ee
Ajeet Singh <Ajeet@123gmail.com>
2026-03-16 21:58:33 +0530

    updated cors-problem

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://yt-genai.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
